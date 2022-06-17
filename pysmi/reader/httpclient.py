pysnmp/pysmi

commit ecc5fb883b563855fe1822af2846c5b72d579d80

fix: replace urllib with requests

pysmi/reader/httpclient.py
@@ -8,13 +8,8 @@ import socket
 import sys
 import time
 
-try:
-    # noinspection PyUnresolvedReferences
-    from urllib2 import Request, urlopen
-except ImportError:
-    # noinspection PyUnresolvedReferences
-    from urllib.request import Request
-    from urllib.request import urlopen
+from requests import session
+
 
 from pysmi.reader.base import AbstractReader
 from pysmi.mibinfo import MibInfo
@@ -32,7 +27,7 @@ class HttpReader(AbstractReader):
     """
     MIB_MAGIC = '@mib@'
 
-    def __init__(self, host, port, locationTemplate, timeout=5, ssl=False):
+    def __init__(self,url):
         """Create an instance of *HttpReader* bound to specific URL.
 
            Note:
@@ -50,10 +45,10 @@ class HttpReader(AbstractReader):
                timeout (int): response timeout
                ssl (bool): access HTTPS web site
         """
-        self._url = '%s://%s:%d%s' % (ssl and 'https' or 'http',
-                                      host, port, decode(locationTemplate))
-
-        socket.setdefaulttimeout(timeout)
+        self._url = url
+     
+        self.session = session()
+        
         self._user_agent = 'pysmi-%s; python-%s.%s.%s; %s' % (
             pysmi_version, sys.version_info[0], sys.version_info[1],
             sys.version_info[2], sys.platform
@@ -81,9 +76,8 @@ class HttpReader(AbstractReader):
             debug.logger & debug.flagReader and debug.logger('trying to fetch MIB from %s' % url)
 
             try:
-                req = Request(url, headers=headers)
-                response = urlopen(req)
-
+                response = self.session.get(url,headers=headers)
+                
             except Exception:
                 debug.logger & debug.flagReader and debug.logger('failed to fetch MIB from %s: %s' % (url, sys.exc_info()[1]))
                 continue

pyproject.toml
@@ -27,6 +27,7 @@ include = ["docs", "tests", "examples", "scripts"]
 [tool.poetry.dependencies]
 python = "^3.7"
 ply = "^3.11"
+requests = "^2.26.0"
 
 
 [tool.poetry.dev-dependencies]

pysmi/reader/url.py
@@ -6,12 +6,7 @@
 #
 import sys
 
-try:
-    # noinspection PyUnresolvedReferences
-    import urlparse
-except ImportError:
-    # noinspection PyUnresolvedReferences
-    from urllib import parse as urlparse
+from urllib import parse as urlparse
 from pysmi.reader.localfile import FileReader
 from pysmi.reader.zipreader import ZipReader
 from pysmi.reader.httpclient import HttpReader
@@ -54,8 +49,7 @@ def getReadersFromUrls(*sourceUrls, **options):
                 readers.append(ZipReader(mibSource.path).setOptions(**options))
 
         elif mibSource.scheme in ('http', 'https'):
-            readers.append(HttpReader(mibSource.hostname or mibSource.netloc, mibSource.port or 80, mibSource.path,
-                                      ssl=mibSource.scheme == 'https').setOptions(**options))
+            readers.append(HttpReader(sourceUrl).setOptions(**options))
 
         else:
             raise error.PySmiError('Unsupported URL scheme %s' % sourceUrl)

commit 3d888269c0e08884eeec72319896e9571b820e2d

getheader() -> headers[]
Regression from ecc5fb883b563855fe1822af2846c5b72d579d80

pysmi/reader/httpclient.py
@@ -89,7 +89,7 @@ class HttpReader(AbstractReader):
 
             if response.status_code == 200:
                 try:
-                    mtime = time.mktime(time.strptime(response.getheader('Last-Modified'), "%a, %d %b %Y %H:%M:%S %Z"))
+                    mtime = time.mktime(time.strptime(response.headers['Last-Modified'], "%a, %d %b %Y %H:%M:%S %Z"))
 
                 except Exception:
                     debug.logger & debug.flagReader and debug.logger('malformed HTTP headers: %s' % sys.exc_info()[1])