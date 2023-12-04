4facd46c498a33df3b873c87f03f9a7245d5f275
ajasnosz <ajasnosz@splunk.com>
2023-12-04 09:40:14 +0100

    fix: update node version

--- .github/workflows/sr-release.sh
+++ .github/workflows/sr-release.sh
@@ -3,5 +3,4 @@
 set -eE
 set -v
 echo pypy user=${PYPI_USERNAME}
-poetry config pypi-token.pypi ${PYPI_TOKEN}
-poetry publish -vvv -n --build
+yes | poetry publish -vvv --build --username ${PYPI_USERNAME} --password ${PYPI_TOKEN}
